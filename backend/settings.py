77b529209cfb79b3a4f6a8db50f19608c5306548
mitul001 <mitulp994@gmail.com>
2024-07-04 21:32:37 +0530

    [IMP] backend: added logic for cache
    - added config about cache in setting
    - added round latitude and longitude logic
    - now able to cache the data

--- backend/settings.py
+++ backend/settings.py
@@ -70,6 +70,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+#cache config
+
+CACHE ={
+    'default':{
+        'BACKEND':'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION':'127.0.0.1:11211',
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,12 @@
 from django.http import JsonResponse
+from django.core.cache import cache
 import requests
+
+def get_region_key(lat,lon,precision=1):
+    """
+    Round the latitude and longitude which helps in grouping nearby locations together
+    """
+    return f"{round(float(lat),precision)}_{round(float(lon),precision)}"
 
 def GetWeather(request):
     apiKey = 'ea52c1dfdd8993a2ea8d63e03a3f159c';
@@ -7,8 +14,19 @@ def GetWeather(request):
     latitude = request.GET.get('lat')
     longitude = request.GET.get('lon')
 
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}&units=metric"
+    region_key = get_region_key(latitude,longitude,precision=1)
+
+    #cache related 
+    cache_key = f"weather_{region_key}"
+    cache_time = 3600
+
+    #check in cache 
+    response = cache.get(cache_key)
+
+    if not response:
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}&units=metric"
     
-    response = requests.get(url).json()
+        response = requests.get(url).json()
+        cache.set(cache_key,response,cache_time)
 
     return JsonResponse(response)
