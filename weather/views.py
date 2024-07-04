b3c74dd5f7d3b7f33853b172bf5289d81c5d2d4b
mitul001 <mitulp994@gmail.com>
2024-07-04 21:22:41 +0530

    [IMP] backend: added a views and url
    - added the api key to the views
    - write a logic to fetch a data from api
    - added corresponding url
    - configure setting.py

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import requests
 
-# Create your views here.
+def GetWeather(request):
+    apiKey = 'ea52c1dfdd8993a2ea8d63e03a3f159c';
+
+    latitude = request.GET.get('lat')
+    longitude = request.GET.get('lon')
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}&units=metric"
+    
+    response = requests.get(url).json()
+
+    return JsonResponse(response)
